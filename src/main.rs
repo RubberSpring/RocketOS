d8ebab8684c62f461c5f0623f0ca2e0658334573
RubberSpring <143825964+rafablog77@users.noreply.github.com>
2026-03-08 15:45:32 -0300

    Formatting changes

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello");
+	println!("Hello");
 
-    loop {}
+	loop {}
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+	println!("{}", info);
+	loop {}
 }
