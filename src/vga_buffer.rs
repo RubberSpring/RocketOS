6b70df322d741af11bc24fb41790b7e747a3e634
RubberSpring <143825964+rafablog77@users.noreply.github.com>
2026-03-15 19:27:10 -0300

    Add tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -38,8 +38,8 @@ impl ColorCode {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
-struct ScreenChar {
-	ascii_character: u8,
+pub struct ScreenChar {
+	pub ascii_character: u8,
 	color_code: ColorCode,
 }
 
@@ -47,14 +47,14 @@ const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
-struct Buffer {
-	chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+pub struct Buffer {
+	pub chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
 	column_position: usize,
 	color_code: ColorCode,
-	buffer: &'static mut Buffer,
+	pub buffer: &'static mut Buffer,
 }
 
 impl fmt::Write for Writer {
